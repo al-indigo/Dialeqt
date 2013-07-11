90b2dbdfb179fcc577f0716db26b464ef1fd63a9
Oleg Borisenko <al@somestuff.ru>
2013-07-11 23:54:49 +0400

    synced with github
    nothing interesting

--- dicttabscontainer.cpp
+++ dicttabscontainer.cpp
@@ -1,10 +1,10 @@
 #include "dicttabscontainer.h"
 #include <QDebug>
-
+#include <QTableView>
 DictTabsContainer::DictTabsContainer(QWidget *parent) :
   QTabWidget(parent)
 {
   qDebug() << "tabs container instantiated";
-  this->addTab(new QWidget(), "test");
+  this->addTab(new QTableView(), "test");
 
 }
